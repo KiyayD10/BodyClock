06b831ba41cbf7ccd5bcc5e98841eaa7252dbfdd
Robani Diansyah <melinting522@gmail.com>
2026-01-03 23:50:15 +0700

    feat: pasang zustand untuk fitur ganti tema dark/light

---
+++ src/store/themeStore.ts
@@ -0,0 +1,24 @@
+import { create } from 'zustand';
+
+//list pilih tema gelap/terang
+type ThemeMode = 'light' | 'dark';
+
+// daftar isi Store: Variable & Function yang tersedia
+interface ThemeStore {
+    mode: ThemeMode;                        // data status dari tema sekarang
+    toggleTheme: () => void;                // function untuk switch Dark - Light mode
+    setTheme: (mode: ThemeMode) => void;    // function buat set manual
+}
+
+export const usethemeStore = create<ThemeStore>((set) => ({
+    mode: 'dark',   // default awal aplikasi gelap
+
+    // logika switch mode
+    toggleTheme: () =>
+        set((state) => ({
+            mode: state.mode === 'dark' ? 'light' : 'dark',
+        })),
+    
+    // Logika ganti tema
+    setTheme: (mode) => set({ mode }),
+})) 
